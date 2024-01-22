f59085f38e52fdb4a964023d346420cfba75999b
Mayank997756 <155308710+Mayank997756@users.noreply.github.com>
2024-01-22 17:46:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/test1.git /LazyPrincess
+  git clone https://github.com/Mayank997756/Manku.git /manku
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /manku
 fi
-cd /LazyPrincess
+cd /manku
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
