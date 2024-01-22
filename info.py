ce25bc4612cdf4821ecaa26dee3f407f643c24e4
Mayank997756 <155308710+Mayank997756@users.noreply.github.com>
2024-01-22 18:57:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,8 +39,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "
-mongodb+srv://mayank9431060:2AfBfiy5Euc3uDXR@mayank9.r7y0cky.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mayank9431060:2AfBfiy5Euc3uDXR@mayank9.r7y0cky.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "mayank9431060")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
